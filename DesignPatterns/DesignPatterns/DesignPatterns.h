9aee046f01aa23f8a9c44d7f55b4679bbf28ba9b
Alex Salom <alex.salom@gmail.com>
2015-11-04 17:01:13 +0100

    Add framework project

---
+++ DesignPatterns/DesignPatterns/DesignPatterns.h
@@ -0,0 +1,19 @@
+//
+//  DesignPatterns.h
+//  DesignPatterns
+//
+//  Created by Alex Salom on 4/11/15.
+//  Copyright © 2015 Alex Salom © alexsalom.es. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DesignPatterns.
+FOUNDATION_EXPORT double DesignPatternsVersionNumber;
+
+//! Project version string for DesignPatterns.
+FOUNDATION_EXPORT const unsigned char DesignPatternsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DesignPatterns/PublicHeader.h>
+
+
